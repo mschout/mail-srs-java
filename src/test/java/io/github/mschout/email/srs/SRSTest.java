e2b14be0d6700922fa4ab4025363573d48075042
Michael Schout <schoutm@gmail.com>
2022-09-17 14:24:59 -0500

    Minor formatting cleanup

---
+++ src/test/java/io/github/mschout/email/srs/SRSTest.java
@@ -0,0 +1,24 @@
+package io.github.mschout.email.srs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SRSTest {
+
+  @Test
+  void guardedSRS() {
+  }
+
+  @Test
+  void isSRS() {
+  }
+
+  @Test
+  void forward() {
+  }
+
+  @Test
+  void reverse() {
+  }
+}
