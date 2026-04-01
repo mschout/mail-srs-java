ae128a8d4c90cc0d6617649cfc17552f94bafff4
Michael Schout <schoutm@gmail.com>
2026-04-01 09:31:16 -0500

    Add version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 java { sourceCompatibility = JavaVersion.VERSION_17 }
 
 dependencies {
-  testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
-  testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+  testImplementation(libs.kotest.runner.junit5)
+  testImplementation(libs.kotest.assertions.core)
 }
 
 // java {
